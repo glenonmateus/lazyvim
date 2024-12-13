04b14df30b795164b94c7738ef8c5f342686a63a
Glenon Mateus <glenonmateus@gmail.com>
2024-12-13 17:49:11 -0300

    feat: add catppuccin color configuration

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,6 @@
 return {
   { "navarasu/onedark.nvim", opts = { style = "darker" } },
-  -- { "folke/tokyonight.nvim", opts = { style = "day" } },
   { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
+  -- { "folke/tokyonight.nvim", opts = { style = "day" } },
+  -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 }
