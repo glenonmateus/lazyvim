983291c30ad91e229fac2a57e08b31bfbe46bc9d
Glenon Mateus <glenonmateus@gmail.com>
2025-01-14 16:57:20 -0300

    feat: setup onedark colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-  { "navarasu/onedark.nvim", opts = { style = "darker" } },
+  { "navarasu/onedark.nvim", opts = { style = "dark" } },
   { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
   -- { "folke/tokyonight.nvim", opts = { style = "day" } },
   -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
