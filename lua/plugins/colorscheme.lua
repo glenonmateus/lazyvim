03f7b811a25b58171a205218a18d788e13c6a664
Glenon Mateus <glenonmateus@gmail.com>
2025-01-14 13:19:19 -0300

    refactor: remove colorscheme

--- lua/plugins/colorscheme.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "navarasu/onedark.nvim", opts = { style = "darker" } },
-  { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
-  -- { "folke/tokyonight.nvim", opts = { style = "day" } },
-  -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
-}
