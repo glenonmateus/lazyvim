db9ddbbc49e66cd75d9854ae36420ea73f8d292b
Glenon Mateus <glenonmateus@gmail.com>
2025-01-14 16:55:06 -0300

    Revert "refactor: remove colorscheme"
    This reverts commit 03f7b811a25b58171a205218a18d788e13c6a664.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,6 @@
+return {
+  { "navarasu/onedark.nvim", opts = { style = "darker" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
+  -- { "folke/tokyonight.nvim", opts = { style = "day" } },
+  -- { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+}
