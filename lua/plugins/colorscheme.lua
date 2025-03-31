6634597075bf4e9a7c7f47ca6ef6f46fe0d8890f
Glenon Mateus <glenonmateus+github@gmail.com>
2025-03-31 14:46:45 -0300

    refactor: change theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,5 +2,9 @@ return {
   -- { "navarasu/onedark.nvim", opts = { style = "darker", transparent = true } },
   -- { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
   -- { "folke/tokyonight.nvim", opts = { style = "day" } },
+  -- {
+  -- "folke/tokyonight.nvim",
+  -- opts = { transparent = true, styles = { sidebars = "transparent", floats = "transparent" } },
+  -- },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
 }
