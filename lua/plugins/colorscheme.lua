d7fad9672815fc7b29697f7736a1d2ee4c0d4280
Glenon Mateus <glenonmateus+github@gmail.com>
2025-04-11 08:14:48 -0300

    refactor: change catppuccin flavour

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,4 @@
 return {
-  -- { "navarasu/onedark.nvim", opts = { style = "darker", transparent = true } },
-  -- { "LazyVim/LazyVim", opts = { colorscheme = "onedark" } },
-  -- { "folke/tokyonight.nvim", opts = { style = "day" } },
-  -- {
-  -- "folke/tokyonight.nvim",
-  -- opts = { transparent = true, styles = { sidebars = "transparent", floats = "transparent" } },
-  -- },
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
+  { "catppuccin/nvim", opts = { flavour = "macchiato" } },
 }
