fde4cd0f0e7d784559ad215d6ea31f7ec8cf491e
Glenon Mateus <glenonmateus+github@gmail.com>
2025-06-17 10:54:57 -0300

    feat(php): add laravel configuration php

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,4 @@
 -- Add any additional options here
 -- vim.opt.wrap = true
 -- vim.g.lazyvim_prettier_needs_config = false
+-- vim.g.lazyvim_php_lsp = "intelephense"
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
 return {
   { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin" } },
-  { "catppuccin/nvim", opts = { flavour = "macchiato" } },
+  -- { "catppuccin/nvim", opts = { flavour = "macchiato" } },
 }
