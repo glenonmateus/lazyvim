0068d65cfde7d84afec0091c8d5734a07e8725dc
Glenon Mateus <glenonmateus+github@gmail.com>
2025-07-16 09:20:22 -0300

    feat: add escape keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
 -- Clear search and stop snippet on escape
-vim.keymap.set("i", "jk", "<esc>", { desc = "Escape" })
+map("i", "jk", "<esc>", { desc = "Escape" })
+
+-- Change LazyVim Change log
+map("n", "<leader>Lc", function()
+  LazyVim.news.changelog()
+end, { desc = "LazyVim Changelog" })
